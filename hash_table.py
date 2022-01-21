1956d9320e8a397c739f03676adac5392361e6cd
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-01-21 20:35:28 +0530

    create hash table

---
+++ hash_table.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+
+class Data:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+
+class HashTable:
+    def __init__(self, table_size):
+        self.table_size = table_size
+        self.hash_table = [None] * table_size
+
+    def custom_hash(self, key):
+        hash_value = 0
+        for i in key:
+            hash_value += ord(i)
+            hash_value = (hash_value * ord(i)) % self.table_size
+        return hash_value
+
+    def add_key_value(self, key, value):
+        hashed_key = self.custom_hash(key)
+        if self.hash_table[hashed_key] is None:
+            self.hash_table[hashed_key] = Node(Data(key, value), None)
+        else:
+            pass
